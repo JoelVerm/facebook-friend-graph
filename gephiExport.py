8c910899f49adac586606682267a84e2ef7ea6f5
Joël Vermeulen <joel.vermeulen5@outlook.com>
2024-07-10 13:10:29 +0200

    skip outliers

--- gephiExport.py
+++ gephiExport.py
@@ -13,15 +13,15 @@ nodes = set()
 friends_connections = {}
 
 for k, v in friend_graph.items():
-    # # If the friend has only one connection, we'll skip it
-    # if len(v) <= 1:
-    #     continue
+    # If the friend has only one connection, we'll skip it
+    if len(v) <= 1:
+        continue
 
     nodes.add(k)
     for item in v:
         edges.add((k, item))
 
-# edges = {(u, v) for u, v in edges if u != v and u in nodes and v in nodes}
+edges = {(u, v) for u, v in edges if u != v and u in nodes and v in nodes}
 
 G = nx.Graph()
 G.add_nodes_from(nodes)
